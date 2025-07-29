d2ecba37d21b64ececff3e89fee169bc3d2f1a5d
D4v1d-MAt30 <david.mateo03@goumh.umh.es>
2025-07-29 11:23:52 +0200

    Add files via upload

--- demos/husky_border_detector.py
+++ demos/husky_border_detector.py
@@ -1,5 +1,5 @@
 import numpy as np
-from RRT import rrt_algorithm
+from RRT import RRT
 from robots.ouster import Ouster
 
 def difference_of_normals(small_radius, large_radius):
@@ -7,7 +7,7 @@ def difference_of_normals(small_radius, large_radius):
     # Copy to preserve the normals for small radius
     normals_small = np.asarray(lidar.pointcloud.normals).copy()
     lidar.estimate_normals(radius=large_radius)
-    normals_large = np.asarray(lidar.pointcloud.normals).copy()
+    normals_large = np.asarray(lidar.pointcloud.normals)
     # Compute the Difference of Normals (DoN)
     diff_norm = (normals_large - normals_small) / 2
     return diff_norm
@@ -44,10 +44,10 @@ def get_edges(lidar, voxel, small_radius, large_radius, min_distance, max_distan
     collision_indices = filter_collision_indices(min_distance, margin, data)
 
     # Colorize: gray for all, red for edges, green for collisions
-    colors = np.full((data.shape[0], 3), 0.5)
-    colors[edge_indices] = [1.0, 0.0, 0.0]         # rojo
-    colors[collision_indices] = [0.0, 1.0, 0.0]  # verde
-    lidar.choose_colors(colors)
+    # colors = np.full((data.shape[0], 3), 0.5)
+    # colors[edge_indices] = [1.0, 0.0, 0.0]         # rojo
+    # colors[collision_indices] = [0.0, 1.0, 0.0]  # verde
+    # lidar.choose_colors(colors)
     # lidar.draw_pointcloud()
 
     # Combine edge and collision indices without removing duplicates
@@ -69,9 +69,22 @@ lidar.from_file("lidar/simulated_pointcloud12.pcd")
 near_edges, near_free = get_edges(lidar, voxel=0.12, small_radius=0.2, large_radius=0.3, min_distance=0, max_distance=5, threshold=0.05, margin=0.5)
 far_edges, far_free = get_edges(lidar, voxel=0.25, small_radius=0.4, large_radius=1, min_distance=5, max_distance=10, threshold=0.15, margin=0.5)
 
+#Add free points that are very close to the robot but aren't seen by lidar
+inside_points = [(0, 0, min(near_free[:, 2]))]
+radius = np.arange(0.2, 2.5, 0.2)
+for r in radius:
+    perimeter = 2 * np.pi * r
+    num_points = int(np.ceil(perimeter / 0.2))
+    for i in range(num_points):
+        theta = 2 * np.pi * i / num_points
+        x = r * np.cos(theta)
+        y = r * np.sin(theta)
+        inside_points.append((x, y, min(near_free[:, 2])))
+np.array(inside_points)
+
 # Combine all edge and free points
 all_edges = np.concatenate((near_edges, far_edges), axis=0)
-all_free = np.concatenate((near_free, far_free), axis=0)
+all_free = np.concatenate((inside_points, near_free, far_free), axis=0)
 
 all_points = np.concatenate((all_edges, all_free), axis=0)
 colors_edges = np.tile([1.0, 0.0, 0.0], (all_edges.shape[0], 1))
@@ -80,9 +93,12 @@ lidar.from_points(all_points)
 colors = np.concatenate((colors_edges, colors_free), axis=0)
 lidar.choose_colors(colors)
 lidar.draw_pointcloud()
-start_point = np.array([3, -3])
-finish_point = np.array([3, 3])
-path = rrt_algorithm(start_point=start_point, goal_point=finish_point, threshold=0.5, goal_threshold=0.25,
-                     incremental_distance=0.5, max_iterations=10000, free_points=all_free[:, :2], obstacle_points=all_edges[:, :2])
-#Save point cloud with the edges in red
-#lidar.save_pointcloud('lidar/simulated_pointcloud7.pcd')
+
+points = np.array([[0, 0], [2, 0], [1, 3], [-5, 1], [6, 3], [6, -3], [6, 9], [6, 0]])
+
+for i in range(len(points)-1):
+    rrt_instance = RRT(points[i], points[i+1], all_free[:, :2], all_edges[:, :2])
+    path_nodes = rrt_instance.run()
+    if path_nodes == [] and i < len(points)-2:
+        rrt_instance = RRT(points[i], points[i + 2], all_free[:, :2], all_edges[:, :2])
+        path_nodes = rrt_instance.run()
